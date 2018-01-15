09383c9a4d445419e744c609facbc9955d0054d8
wendyzou02 <wendyzou02@gmail.com>
2018-01-15 11:33:18 -0800

    test commit

--- demolib.py
+++ demolib.py
@@ -1,6 +1,6 @@
 def sum_even(start_num, end_num):
     sum = 0
-    for x in range( start_num, end_num+1 ):
+    for x in range( start_num, end_num ):
         if x%2==0:
             sum += x
     return sum
