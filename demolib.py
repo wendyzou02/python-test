04760f049c8adba2e509a9e90720a65f1aa394f3
Whimsical Pulp <whimsical.pulp@gmail.com>
2018-01-14 21:16:26 -0800

    fixes range bug

--- demolib.py
+++ demolib.py
@@ -1,6 +1,6 @@
 def sum_even(start_num, end_num):
     sum = 0
-    for x in range( start_num, end_num ):
+    for x in range( start_num, end_num+1 ):
         if x%2==0:
             sum += x
     return sum
